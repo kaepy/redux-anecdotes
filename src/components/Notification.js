f719bd88792205540c90b7ab1ebc9c7c3ebe2cbc
kaepy <henna.tuunainen@gmail.com>
2024-09-19 15:45:53 +0300

    6.3 anekdootit, step 1: anekdoottien äänestys toiminnallisuus reduxin avulla

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,14 +1,11 @@
 const Notification = () => {
   const style = {
-    border: 'solid',
+    border: "solid",
     padding: 10,
-    borderWidth: 1
-  }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
-}
+    borderWidth: 1,
+  };
 
-export default Notification
+  return <div style={style}>render here notification...</div>;
+};
+
+export default Notification;
